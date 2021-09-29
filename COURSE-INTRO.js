503d6a36064d3df8026d41b28a15f0a9b03c179f
Blaire Baker <bakerblaire@gmail.com>
2021-09-28 21:31:46 -0600

    HTML boilerplate

---
+++ COURSE-INTRO.js
@@ -0,0 +1,18 @@
+// HOW DOES THE INTERNET WORK?
+
+    // The Internet: a long piece of wire that connects different computers to each other
+    // Server: a library computer that holds the information
+    // Client: the user's computer
+    // Browser sends message to internet service provider with URL
+    // Service provider sends that request to DNS Server, which looks up exact IP address
+    // DNS server sends IP address back to Client, then a direct request is sent through internet 
+    // service provider through the internet backbone
+    // Server has all the files, sends them back to client through backbone
+
+// HOW DO WEBSITES WORK?
+
+    // Browser: used to access a web page (Chrome, Firefox, Safari, etc.)
+    // Code Files: HTML, CSS, and JavaScript
+    // HTML: the structure
+    // CSS: the styling
+    // JavaScript: 
